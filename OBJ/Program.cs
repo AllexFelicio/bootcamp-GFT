cc7c304f1b5ce28022bab4d2995b492f140666f7
Alex Sander <33581240+AllexFelicio@users.noreply.github.com>
2022-05-09 13:13:47 -0300

    Add files via upload

---
+++ OBJ/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+using introdução_obj.src.Entities;
+
+namespace dotnet_poo
+{
+    class Program
+    {
+
+        static void Main(){
+
+                Knight hero = new Knight("Arus",24,"Knight");
+                Wizard wizard = new Wizard("Jennica",23,"White Wizard");
+
+              WriteLine(hero.Attack());
+              WriteLine(wizard.Attack(9));
+
+
+
+        }
+
+    }
+
+
+
+
+}
